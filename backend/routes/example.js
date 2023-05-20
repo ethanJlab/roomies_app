e5179f96833361ee0cb26e24078478d61d42d733
ethanJlab <ethanjanicki89@gmail.com>
2023-05-20 15:38:10 -0400

    establish DB connection

--- backend/routes/example.js
+++ backend/routes/example.js
@@ -1,12 +1,38 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
-import client from '../index.js';
+import {MongoClient, ServerApiVersion} from 'mongodb';
 export const exampleRouter = express.Router();
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri,  {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+}
+);
+await client.connect();
 
 exampleRouter.post('/', (req, res) => {
     console.log("Hello Wolrld!!");
     res.send("Hello World!!");
+});
+
+exampleRouter.get('/', (req, res) => {
+    let temp = client.db("roomies-app-db").collection("Users").find({}).toArray()
+    .then((result) => {
+        console.log(result);
+        res.send(result);
+    })  
+    .catch((err) => {
+        console.log(err);
+
+    });
 });
 
 
